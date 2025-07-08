32ad84bd8ad12cf52116933ad6df846ee5efb960
ipscoder <psawant0015@gmail.com>
2025-07-08 12:01:22 +0530

    feat:minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/common/header/Header";
 import HomePage from "./(public)/home";
+import Footer from "@/components/common/footer/Footer";
 
 export default function Home() {
   return (
+    <>
+     <Header />
    <HomePage/>
+   <Footer/>
+    </>
   );
 }
